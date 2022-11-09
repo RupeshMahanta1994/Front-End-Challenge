4e7d41b12b03752a12ef17f806c1fd7832ff1522
sohanmahanta <117063501+RupeshMahanta1994@users.noreply.github.com>
2022-11-09 12:56:50 +0530

    name upadate

---
+++ patientRegistration/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+      <h1>hello world i love billi</h1>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ patientRegistration/src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <div className="navbar">
+                <div className="leftSide"></div>
+                <div className="rightSide"></div>
+            </div>
+
+
+
+        </>
+    )
+}
+
+export default Navbar
