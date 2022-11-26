c46153542a5399cf499f0f4e7d30d4e644f62928
sohanmahanta <117063501+RupeshMahanta1994@users.noreply.github.com>
2022-11-26 18:10:09 +0530

    Event Website

--- portfolio/src/components/MyIntro.jsx
+++ portfolio/src/components/MyIntro.jsx
@@ -21,7 +21,31 @@ function MyIntro() {
 
                 </div>
                 <div className="container" id='aboutMe'>
-                    About Me
+                    <div className="aboutme">
+                        <div className="heading"><h2>about me</h2></div>
+                        <div className="personal-details">
+                            <table >
+                                <tr>
+                                    <td>Name:</td>
+                                    <td>Rupesh Mahanta</td>
+                                </tr>
+                                <tr>
+                                    <td>Email:</td>
+                                    <td>mahanta.rupesh704@gmail.com</td>
+                                </tr>
+                                <tr>
+                                    <td>Phone:</td>
+                                    <td>(+91)6206679616</td>
+                                </tr>
+                                <tr>
+                                    <td>Address:</td>
+                                    <td>Rourkela</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div className="short-description">Hello my name is Rupesh  mahanata
+                            i an a front end developer, in proficeent in HTML CSS & React.Js</div>
+                    </div>
                 </div>
                 <div className="container" id='education'>
                     education
